baf467b3acd532a3ae992760b7afa42bfb2b8803
igoradm <igorpetrov42ru@gmail.com>
2024-01-03 16:09:23 +0300

    куда то слетела вся основа, создал новую, нулевую

---
+++ WebAPI_Architecture/WebAPI/Domain/Class1.cs
@@ -0,0 +1,16 @@
+﻿namespace Domain;
+
+public class Class1
+{
+    public Guid UserId { get; set; }
+    
+    public Guid Id { get; set; } 
+    
+    public string Title { get; set;  }
+    
+    public string Details { get; set; }
+    
+    public DateTime DateCreation { get; set; }
+    
+    public DateTime? DateEdit { get; set; }
+}
