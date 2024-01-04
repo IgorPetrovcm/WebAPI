e30b032a85af92066a077fccbdab95aebd9b27a3
igoradm <igorpetrov42ru@gmail.com>
2024-01-04 22:30:50 +0300

    add database, very hard...

--- MinimalAPIs_Project/WebAPI/Program.cs
+++ MinimalAPIs_Project/WebAPI/Program.cs
@@ -1,28 +1,71 @@
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
 
-List<User> users = new List<User>();
-
-app.MapGet("/users", () => users);
-
-app.MapGet("/users/{id}", (int id) => users.FirstOrDefault(x => x.Id == id));
-
-app.MapPost("/users", (User user) => users.Add(user));
-
-app.MapPut("/users/{id}", (User user, int id) => 
+builder.Services.AddDbContext<UserDb>(options => 
 {
-    int index = users.FindIndex(x => x.Id == id);
-
-    users[index] = user;
+    options.UseSqlite(builder.Configuration.GetConnectionString("Sqlite"));
 });
 
-app.MapDelete("/users/{id}", (int id) => {
-    User user = users.FirstOrDefault(x => x.Id == id);
-    users.Remove(user);
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    using var scope = app.Services.CreateScope();
+    var db = scope.ServiceProvider.GetService<UserDb>();
+
+    db.Database.EnsureCreated();
+}
+
+app.MapGet("/users", async (UserDb db) => await db.users.ToListAsync());
+
+app.MapGet("/users/{id}", async (int id, [FromServices]UserDb db) => {
+    User user = await db.users.FirstOrDefaultAsync(x => x.Id == id);
+    if (user == null) return Results.NotFound();
+    return Results.Ok(user);
+});
+
+app.MapPost("/users", async ([FromBody]User user, [FromServices]UserDb db) =>{
+    if (user != null)
+    {
+        await db.users.AddAsync(user);
+        await db.SaveChangesAsync();
+        Results.Created($"/users/{user.Id}",user);
+    }
+    else Results.NotFound();
+});
+
+app.MapPut("/users/{id}", async ([FromBody]User user,int id, [FromServices]UserDb db) => 
+{
+    User currentUser = await db.users.FirstOrDefaultAsync(x => x.Id == id);
+    if(currentUser != null)
+    {
+        currentUser.Age = user.Age;
+        currentUser.Name = user.Name;
+        db.users.Update(currentUser);
+        await db.SaveChangesAsync();
+        Results.NoContent();
+    }
+    else Results.NotFound();
+});
+
+app.MapDelete("/users/{id}", async (int id,[FromServices]UserDb db) => {
+    User user = await db.users.FirstOrDefaultAsync(x => x.Id == id);
+    if (user != null)
+    {
+        db.users.Remove(user);
+        await db.SaveChangesAsync();
+        Results.NoContent();
+    }
+    else Results.NotFound();
 });
 
 app.Run();
 
+public class UserDb : DbContext
+{
+    public UserDb(DbContextOptions<UserDb> options) : base(options) {}
+
+    public DbSet<User> users => Set<User>();
+}
 public class User
 {
     public int Id {get;set;}
