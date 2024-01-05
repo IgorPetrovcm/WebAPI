1963d6d9b773d719d9d29bbf2909250850b02091
igoradm <igorpetrov42ru@gmail.com>
2024-01-05 20:36:14 +0300

    основы webAPI with controllers

--- WebAPI/FirstStep/Controllers/UserController.cs
+++ WebAPI/FirstStep/Controllers/UserController.cs
@@ -1,11 +1,11 @@
-namespace FirstStep;
+namespace FirstStep.Controllers;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using FirstStep.Models;
 using System.ComponentModel;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using System.Reflection.Metadata.Ecma335;
 
-[ApiController]
-[Route("/[controllers]")]
 public class UserController : ControllerBase
 {
     private readonly UserContext _context;
@@ -17,11 +17,20 @@ public class UserController : ControllerBase
         _context.Database.EnsureCreated();
 
         _context.Users.Add(new User{Name = "Igor", Age = 18});
+
+        _context.SaveChanges();
     }
 
     [HttpGet]
     public async Task<List<User>> Get()
     {
         return await _context.Users.ToListAsync();
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetById(int id)
+    {
+        User user = await _context.Users.FirstOrDefaultAsync(x => x.Id == id) ;
+        return user is not null ? Ok(user) : BadRequest();
     }
 }
--- WebAPI/FirstStep/Program.cs
+++ WebAPI/FirstStep/Program.cs
@@ -3,12 +3,20 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllers();
+
 builder.Services.AddDbContext<UserContext>(option => {
     option.UseSqlite("Data Source=users.db");
 });
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+
+app.MapControllerRoute("default", "{controller=User}/{action=Get}");
+
+app.MapControllerRoute(
+    name: "GetUserById",
+    pattern: "{controller=User}/{action=GetById}/{id:int}"
+);
 
 app.Run();
