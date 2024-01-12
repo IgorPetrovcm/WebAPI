db88a084e22c79a5079e2b0faab07066580df074
IgorPetrovcm <igorpetrov42ru@gmail.com>
2024-01-12 21:08:36 +0300

    add api-controllers and controllers that return views

---
+++ WebAPI/Authorization/Authorization.WebAPIs/Controllers/ApiUserAuth.cs
@@ -0,0 +1,46 @@
+using Authorization.Application.Interfaces.DTOs;
+
+namespace WebApplication1.Controllers;
+
+using Microsoft.AspNetCore.Mvc;
+using Authorization.Application.Interfaces.Services; 
+using Authorization.Application.DTOs;
+using Authorization.Persistence;
+using Authorization.Persistence.Repository;
+using Authorization.Infrastructure.Services;
+
+
+[ApiController]
+[Route("api/UserAuth")]
+public class ApiUserAuth : ControllerBase
+{
+    private UserRepository _rep;
+    
+    public ApiUserAuth(IConfigurationManageService config, ApplicationContext context)
+    {
+        _rep = new UserRepository(context, config.GetConnectionString("ApiSettings", "SecretKey",
+            "C:\\Users\\igorp\\Programming\\WebAPI\\WebAPI\\Authorization\\Authorization.WebAPIs\\appsettings.json"));
+    }
+    
+    [HttpPost("Login")]
+    public async Task<IActionResult> Login([FromBody]RequestLoginDTO userRequest)
+    {
+        ResponseLoginDTO response = await _rep.Login(userRequest);
+
+        if (response.User == null || response.Token == "")
+            return BadRequest(new { message = "This user not found " });
+
+        return Ok(response);
+    }
+
+    [HttpPost("Register")]
+    public async Task<IActionResult> Register([FromBody]RequestRegisterationDTO userRequest)
+    {
+        if (_rep.IsUniqueUser(userRequest.Login))
+        {
+            return Ok(await _rep.Registeration(userRequest));
+        }
+
+        return BadRequest(new { message = "Login is not unique" });
+    }
+}
--- WebAPI/Authorization/Authorization.WebAPIs/Controllers/UserAuth.cs
+++ WebAPI/Authorization/Authorization.WebAPIs/Controllers/UserAuth.cs
@@ -1,46 +1,15 @@
-using Authorization.Application.Interfaces.DTOs;
-
 namespace WebApplication1.Controllers;
 
 using Microsoft.AspNetCore.Mvc;
-using Authorization.Application.Interfaces.Services; 
-using Authorization.Application.DTOs;
-using Authorization.Persistence;
-using Authorization.Persistence.Repository;
-using Authorization.Infrastructure.Services;
 
 
-[ApiController]
-[Route("api/UserAuth")]
 public class UserAuth : Controller
 {
-    private UserRepository _rep;
-    
-    public UserAuth(IConfigurationManageService config, ApplicationContext context)
+    public IActionResult Register() => View();
+
+    [HttpPost]
+    public IActionResult Register([FromBody] string path)
     {
-        _rep = new UserRepository(context, config.GetConnectionString("ApiSettings", "SecretKey",
-            "C:\\Users\\igorp\\Programming\\WebAPI\\WebAPI\\Authorization\\Authorization.WebAPIs\\appsettings.json"));
-    }
-    
-    [HttpPost("Login")]
-    public async Task<IActionResult> Login([FromBody]RequestLoginDTO userRequest)
-    {
-        ResponseLoginDTO response = await _rep.Login(userRequest);
-
-        if (response.User == null || response.Token == "")
-            return BadRequest(new { message = "This user not found " });
-
-        return Ok(response);
-    }
-
-    [HttpPost("Register")]
-    public async Task<IActionResult> Register([FromBody]RequestRegisterationDTO userRequest)
-    {
-        if (_rep.IsUniqueUser(userRequest.Login))
-        {
-            return Ok(_rep.Registeration(userRequest));
-        }
-
-        return BadRequest(new { message = "Login is not unique" });
+        return RedirectPermanent(path);
     }
 }
